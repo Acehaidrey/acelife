9444fd653f7c46ff5e5a1f79f359cc18229d88c4
Ace Haidrey <acehaidrey@gmail.com>
2023-03-05 22:00:30 -0800

    Add google app scripts to project

---
+++ GoogleAppScripts/archiveOldOrderEmails.js
@@ -0,0 +1,137 @@
+const defaultDaysAgoCutoff = 7;
+const orderLabel = 'Orders';
+const billingLabel = 'Billings';
+const statsMap = {};
+
+const Platform = {
+  SLICE: 'Slice',
+  MENUSTAR: 'Menustar',
+  DOORDASH: 'Doordash',
+  MENUFY: 'Menufy',
+  EATSTREET: "Eatstreet",
+  GRUBHUB: "Grubhub",
+  TOAST: "Toast",
+  BRYGID: "Brygid",
+  SPEEDLINE: "Speedline",
+  OFFICE_EXPRESS: "OfficeExpress",
+  BEYONDMENU: "BeyondMenu"
+};
+
+/**
+ * Function to clean up old order emails to archive and mark as read to reduce email box clutter.
+ * This function is predecated that order labels are properly assigned.
+ * The easier implementation will be to go through all Orders/* labels but App Script timeout
+ * occurs in this instance. Therefore require manually invoking on each partner and have one catch all
+ * function for the partners that are not manually added here.
+ * NOTE, this gets the last 500 threads only. Larger sizes can use while loop with label.getThreads(start, maxThreads).
+ */
+function archiveOldOrderEmails(userLabel, daysAgoCutoff = defaultDaysAgoCutoff) {
+  const startTime = Date.now();
+  const daysAgo = new Date(Date.now() - daysAgoCutoff * 24 * 60 * 60 * 1000);
+  const label = GmailApp.getUserLabelByName(userLabel);
+  // if label not found in our gmail box error out
+  if (!label) {
+    throw new Error(`ERROR no label found for ${label}`)
+  }
+  const labelName = label.getName();
+  statsMap[labelName] = 0;
+  const threads = label.getThreads();
+  Logger.log(`${labelName}, ${threads.length} emails threads checking to archive.`);
+  for (var j = 0; j < threads.length; j++) {
+        const currThread = threads[j];
+        // if message is in inbox means not archived - mark read and archive it
+        if (currThread.isInInbox() && currThread.getLastMessageDate() > daysAgo) {
+          currThread.markRead();
+          currThread.moveToArchive();
+          statsMap[labelName] += 1;
+        }
+  }
+  const elapsedTime = (Date.now() - startTime) / 1000;
+  Logger.log(`${labelName}, ${statsMap[labelName]} emails archived and marked as read.`);
+  Logger.log(`Total elapsed time so far: ${elapsedTime} seconds.`);
+}
+
+// BeyondMenu
+function archiveOldOrderEmailsBeyondMenu() {
+  const userLabel = `${orderLabel}/${Platform.BEYONDMENU}`;
+  archiveOldOrderEmails(userLabel);
+}
+
+// Brygid
+function archiveOldOrderEmailsBrygid() {
+  const userLabel = `${orderLabel}/${Platform.BRYGID}`;
+  archiveOldOrderEmails(userLabel);
+}
+
+// Doordash
+function archiveOldOrderEmailsDoordash() {
+  const userLabel = `${orderLabel}/${Platform.DOORDASH}`;
+  archiveOldOrderEmails(userLabel);
+}
+
+// Eatstreet
+function archiveOldOrderEmailsEatstreet() {
+  const userLabel = `${orderLabel}/${Platform.EATSTREET}`;
+  archiveOldOrderEmails(userLabel);
+}
+
+// Grubhub
+function archiveOldOrderEmailsGrubhub() {
+  const userLabel = `${orderLabel}/${Platform.GRUBHUB}`;
+  archiveOldOrderEmails(userLabel);
+}
+
+// Menufy
+function archiveOldOrderEmailsMenufy() {
+  const userLabel = `${orderLabel}/${Platform.MENUFY}`;
+  archiveOldOrderEmails(userLabel);
+}
+
+// Menustar
+function archiveOldOrderEmailsMenustar() {
+  const userLabel = `${orderLabel}/${Platform.MENUSTAR}`;
+  archiveOldOrderEmails(userLabel);
+}
+
+// OfficeExpress
+function archiveOldOrderEmailsOfficeExpress() {
+  const userLabel = `${orderLabel}/${Platform.OFFICE_EXPRESS}`;
+  archiveOldOrderEmails(userLabel);
+}
+
+// Slice
+function archiveOldOrderEmailsSlice() {
+  const userLabel = `${orderLabel}/${Platform.SLICE}`;
+  archiveOldOrderEmails(userLabel);
+}
+
+// Toast
+function archiveOldOrderEmailsToast() {
+  const userLabel = `${orderLabel}/${Platform.TOAST}`;
+  archiveOldOrderEmails(userLabel);
+}
+
+// All Others
+function archiveOldOrderEmailsOthers() {
+  const platforms = Object.values(Platform);
+  const labels = GmailApp.getUserLabels();
+  for (var i = 0; i < labels.length; i++) {
+    const labelName = labels[i].getName();
+    if ((labelName.indexOf(orderLabel + "/") > -1 || labelName === orderLabel) && !platforms.includes(labelName.replace(orderLabel + '/', ''))) {
+      Logger.log(`${labelName} missing from the manual checks. Caught in the Others call.`)
+      archiveOldOrderEmails(labelName);
+    }
+  }
+}
+
+// Grubhub Billing
+function archiveOldBillingEmailsGrubhub() {
+    const userLabel = `${billingLabel}/${Platform.GRUBHUB}`;
+    archiveOldOrderEmails(userLabel, 30);
+}
+
+// Toast Billing
+function archiveOldBillingEmailsToast() {
+    const userLabel = `${billingLabel}/${Platform.TOAST}`;
+    archiveOldOrderEmails(userLabel, 30);
+}
---
+++ GoogleAppScripts/grubhubAutoContest.js
@@ -0,0 +1,175 @@
+const grubhubContestLabel = 'Billings/Grubhub/Adjustments';
+const grubhubSenderEmail = 'restaurants@grubhub.com';
+const driveFolderName = 'GrubhubAdjustments';
+const daysAgoCutoffForContest = 1;
+
+/**
+ * Save the email attachment passed to Drive and convert the PDF to a Text file.
+ * @param {*} attachment 
+ * @returns {string} - PDF content as text
+ */
+function saveAttachmentToDriveAndGetText(attachment) {
+  // create folder if not exists
+  let folder = DriveApp.getFoldersByName(driveFolderName);
+  if (!folder) {
+    DriveApp.createFolder(driveFolderName);
+  }
+  // save the PDF from email to Drive
+  folder = DriveApp.getFoldersByName(driveFolderName).next();
+  var fileBlob = attachment.copyBlob();
+  var fileName = attachment.getName();
+  var file = folder.createFile(fileBlob).setName(fileName);
+  // use OCR to convert PDF -> Text file
+  const { id } = Drive.Files.insert(
+    {
+      title: file.getName().replace(/\.pdf$/, ''),
+      mimeType: file.getMimeType(),
+    },
+    fileBlob,
+    {
+      ocr: true,
+      ocrLanguage: 'en',
+      fields: 'id',
+    }
+  );
+  const textContent = DocumentApp.openById(id).getBody().getText();
+  DriveApp.getFileById(id).setTrashed(true);
+  return textContent;
+}
+
+/**
+ * Parse the email PDF converted text file and get the relevant information for contesting information.
+ * @param {string} pdfText 
+ * @returns {*[]}
+ */
+function createEmailContext(pdfText) {
+    const disputeRecords = [];
+    const pdfTextList = pdfText.split('\n').map((value) => value.trim());
+    const restaurant = pdfTextList[1];
+    // specific to our case where always have zipcode as 92630 to get address
+    const cityInfoIndex = pdfTextList.findIndex(item => item.includes('92630'));
+    const address = pdfTextList[cityInfoIndex - 1];
+    const cityInfo = pdfTextList[cityInfoIndex];
+    // find where Order Detail is in the output then get all values after it
+    let orderDetailText = pdfText.substring(pdfText.indexOf("Order Detail") + "Order Detail".length).trim();
+    // remove the last line as it is the total day summary of order details
+    orderDetailText = orderDetailText.replace(pdfTextList[pdfTextList.length - 1], '');
+    // find Total in the result
+    const totalIndex = orderDetailText.indexOf("Total");
+    // const keys = orderDetailText.substring(0, totalIndex + "Total".length).split(' ');
+    let recordsText = orderDetailText.substring(totalIndex + "Total".length, orderDetailText.length).trim();
+    // split the line by white space for each record and split records by the orderId that is just a single line remove it
+    recordsText = recordsText.split('\n')
+    .filter(line => {
+      const words = line.trim().split(' ');
+      return !line.match(/[A-Z]-\d+/) || words.length > 1;
+    });
+    recordsText = recordsText.join(' ').replace(/\s+/g,' ');
+    recordsText = recordsText.split(/([A-Z]-\d+)/).filter(Boolean);
+    // reduce so we have combined record like below
+    // O-783021753230271 Cancelation of 02/26/23 10:15 PM -$24.74 $.00 -$1.92 $.00 -$26.66
+    // filter out Adjust and Cancel orders only
+    const groupedRows = recordsText.reduce((acc, cur, i) => {
+      if (i % 2 === 0) {
+        acc.push([cur.trim(), recordsText[i+1].trim()]);
+      }
+      return acc;
+    }, []).map(arr => arr.join(' ')).filter(row => row.includes('Adjust') || row.includes('Cancel'));
+    for (var i = 0; i < groupedRows.length; i++) {
+      const splitRow = groupedRows[i].replace(/\s+/g,' ').trim().split(' ');
+      // parse the record to best of our ability for info we need
+      const record = {
+        ID: splitRow[0],
+        Type: groupedRows[i].includes('Adjust') ? 'adjustment' : 'cancellation',
+        Date: groupedRows[i].match((/\d{2}\/\d{2}\/\d{2}/))[0],
+        Time: groupedRows[i].match(/\d{1,2}:\d{2}\s(AM|PM)/)[0],
+        Subtotal: splitRow[splitRow.length - 5],
+        Delivery: splitRow[splitRow.length - 4],
+        Tax: splitRow[splitRow.length - 3],
+        Tip: splitRow[splitRow.length - 2],
+        Total: splitRow[splitRow.length - 1],
+        Restaurant: restaurant,
+        Address: address + ', ' + cityInfo,
+        Provider: 'Grubhub'
+      };
+      disputeRecords.push(record);
+    }
+    return disputeRecords;
+}
+
+/**
+ * For each of the dispute records made, send out an email to support to contest the issue.
+ * @param {*[]} disputes 
+ * @param {string} email
+ */
+function createEmail(disputes, email) {
+    if (!disputes || disputes.length === 0) {
+        Logger.info('No dispute records exist')
+        throw new Error('No dispute records exist')
+    }
+    for (var i = 0; i < disputes.length; i++) {
+        const record = disputes[i];
+        const subject = `Adjustment Contest Order ${record.ID}`
+        const body = `Dear ${record.Provider} Customer Service,
+
+        I am writing on behalf of my restaurant, ${record.Restaurant}, located at ${record.Address}, 
+        to address the ${record.Type} to order ${record.ID} on ${record.Date}. That have been charged 
+        to our account. We need to contest these charges and inform you that we have done our due 
+        diligence in ensuring that all orders are fulfilled to the best of our ability.
+        
+        We take pride in providing high-quality food and excellent customer service to our valued 
+        customers, which is why we take every order seriously. We make sure to read all instructions 
+        carefully, verify all details with the customer over the phone, and get signature sign-off 
+        from the driver to confirm that all items have been delivered.
+        
+        Despite our efforts, we have been charged adjustment fees for these orders in the amount of 
+        ${record.Total}. We believe that these charges are unwarranted, as we have done everything 
+        in our power to fulfill the orders to the best of our ability. We cannot accept this.
+        
+        We kindly request that you urgently review and pay us back the adjustment fees, as well as 
+        any delivery fees that may apply, ${record.Delivery}. We value our partnership with ${record.Provider} 
+        and hope to continue to provide our customers with high-quality food and exceptional service.
+        
+        Thank you for your attention to this matter. Especially in these times.
+        
+        Sincerely,
+    
+        Ace Haidrey
+        ${record.Restaurant}
+        `
+        GmailApp.sendEmail(email, subject, body);
+    }
+}
+
+/**
+ * Main entry point to contest Grubhub orders based off of the email record they send daily.
+ */
+function processGrubhubOrderContests() {
+  const daysAgo = new Date(Date.now() - daysAgoCutoffForContest * 24 * 60 * 60 * 1000);
+  const label = GmailApp.getUserLabelByName(grubhubContestLabel);
+  const threads = label.getThreads();
+  for (var i = 0; i < threads.length; i++) {
+    // get all messages in the last N days
+    if (threads[i].getLastMessageDate() > daysAgo) {
+      var messages = threads[i].getMessages();
+      for (var j = 0; j < messages.length; j++) {
+        const attachments = messages[j].getAttachments();
+        // if no attachments
+        if (!attachments || attachments.length === 0) {
+          continue;
+        }
+        for (var k = 0; k < attachments.length; k++) {
+          Logger.log(attachments[k].getName())
+          // no library to read PDF directly - save to Drive and leverage there to read to text
+          if (attachments[k].getContentType() === 'application/pdf') {
+            const text = saveAttachmentToDriveAndGetText(attachments[k]);
+            const disputeRecs = createEmailContext(text);
+            Logger.log(text);
+            Logger.log(disputeRecs);
+            createEmail(disputeRecs, grubhubSenderEmail);
+          }
+        }
+      }
+    }
+  }
+}
