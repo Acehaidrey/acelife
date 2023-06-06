9da5a28c0253572026b5f342b3b2fcc5e854659a
Ace Haidrey <acehaidrey@gmail.com>
2023-06-06 12:58:14 -0700

    fix futurefoods

--- provider_reports/providers/futurefoods.py
+++ provider_reports/providers/futurefoods.py
@@ -1,21 +1,33 @@
-import csv
+import glob
 import os
+import re
+import shutil
 from datetime import datetime
 import time
 
 import pandas as pd
 import retrying
-from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 from provider_reports.providers.base_provider import OrdersProvider
 from provider_reports.utils.constants import Store, RAW_REPORTS_PATH, Provider, ReportType, Extensions
 from provider_reports.utils.utils import get_chrome_options
 from provider_reports.utils.validation_utils import ValidationUtils
+
+
+def get_date_from_aria_label(element):
+    date_pattern = r"(?:Choose\s)(.*?)(?:\sas)"
+    aria_label = element.get_attribute('aria-label')
+    if not aria_label:
+        return
+    match = re.search(date_pattern, aria_label)
+    if match:
+        date_string = match.group(1)
+        dt = datetime.strptime(date_string, "%A, %B %d, %Y")
+        return dt
 
 
 class FutureFoodsOrders(OrdersProvider):
@@ -60,173 +72,142 @@ class FutureFoodsOrders(OrdersProvider):
         password_input.send_keys(self.password)
         self.wait.until(EC.presence_of_element_located((By.XPATH, "//button/div[contains(.,'Sign in')]"))).click()
 
-    # @retrying.retry(wait_fixed=5000, stop_max_attempt_number=3)
     def get_orders(self):
+        start_time = time.time()
+        formatted_start_date = self.start_date_dt.strftime("%A, %B {day}, %Y".format(day=self.start_date_dt.day))
+        formatted_end_date = self.end_date_dt.strftime("%A, %B {day}, %Y".format(day=self.end_date_dt.day))
         # go to virtual brands endpoint where reporting exists
         self.driver.get('https://manager.tryotter.com/virtual-brands/accounting/payments')
         button = self.wait.until(EC.presence_of_element_located((By.XPATH, "//button[contains(., 'Download CSV')]")))
         button.click()
         input_field = self.wait.until(EC.presence_of_element_located((By.XPATH, "//input[@placeholder='Select dates']")))
-        # input_field.click()
-
-        # Specify the new value to set
-        new_value = "Apr 1, 2023 - Apr 30, 2023"
-
-        # # Find the input element by its attributes
-        # input_element = driver.find_element(By.CSS_SELECTOR,
-        #                                     'input[placeholder="Select dates"][data-testid="op-input"]')
-
-        # Clear the existing value
-        input_field.clear()
-
-        # Set the new value
-        input_field.send_keys(new_value)
-        # Execute JavaScript to set the value of the input field
-        script = f'document.querySelector(\'input[placeholder="Select dates"][data-testid="op-input"]\').value = "{new_value}";'
-        self.driver.execute_script(script)
         input_field.click()
 
-        time.sleep(5)
+        # have the calendar modal opened
+        # identify the back and forward button elements
+        buttons = self.driver.find_elements(By.CSS_SELECTOR, 'div[role="button"][class*="DayPickerNavigation_button"]')
+        # self.wait.until(EC.visibility_of_element_located(buttons[0]))
+        back_button, forward_button = None, None
+        for btn in buttons:
+            if btn:
+                aria_label = btn.get_attribute("aria-label") or ''
+                if "previous month" in aria_label.lower():
+                    back_button = btn.find_element(By.TAG_NAME, 'svg')
+                if "next month" in aria_label.lower():
+                    forward_button = btn.find_element(By.TAG_NAME, 'svg')
 
-        # download_button = self.driver.find_element(By.XPATH, "//button[text()='Download']")
-        # try:
+        # get active day elements
+        elements = self.driver.find_elements(By.XPATH,
+                                        '//td[contains(@class, "CalendarDay") and @role="button" and @aria-disabled="false"]')
+        first_active_date_td = elements[0]
+        last_active_date_td = elements[-1]
+        if not first_active_date_td and last_active_date_td:
+            raise Exception('No active day elements found')
+
+        first_date = get_date_from_aria_label(first_active_date_td)
+        last_date = get_date_from_aria_label(last_active_date_td)
+        print(self.start_date_dt, first_date, first_active_date_td.get_attribute('aria-label'))
+        print(self.end_date_dt, last_date, last_active_date_td.get_attribute('aria-label'))
+        print(first_active_date_td.is_displayed())
+        print(last_active_date_td.is_displayed())
+
+        # debug html
+        # Save the HTML source to a local file
+        with open(os.path.join(RAW_REPORTS_PATH, 'futurefoods.html'), 'w+', encoding='utf-8') as file:
+            file.write(self.driver.page_source)
+            print(f"HTML source saved to '{os.path.join(RAW_REPORTS_PATH, 'futurefoods.html')}'")
+
+        # if the first date is greater than start_date then we need to go back
+        # if the start_element is not set or not visible then click back
+        start_element = None  # first_date > self.start_date_dt
+        while not start_element:
+            elements = self.driver.find_elements(By.XPATH,
+                                                 '//td[contains(@class, "CalendarDay") and @role="button" and @aria-disabled="false"]')
+            # first_active_date_td = elements[0]
+            # first_date = get_date_from_aria_label(first_active_date_td)
+            try:
+                ele = self.driver.find_element(By.XPATH, f'//td[contains(@aria-label, "{formatted_start_date}")]')
+                print('found ele')
+                print(ele.is_displayed())
+                print(ele.get_attribute('aria-label'))
+                print(ele.get_attribute('outerHTML'))
+                if ele.is_displayed():
+                    start_element = ele
+            except Exception as e:
+                print(e)
+                print('start_element not found or not visible')
+                start_element = None
+            back_button.click()
+
+        # select the start_date
+        try:
+            # start_element = self.driver.find_element(By.XPATH, f'//td[contains(@aria-label, "{formatted_start_date}")]')
+            # print(start_element.get_attribute('innerHTML'))
+            start_element.click()
+            print('found matching element')
+        except Exception as e:
+            print('could not find start date')
+            print(e)
+
+        print('all elements')
+        for ele in elements:
+            try:
+                print(ele.get_attribute('aria-label') + ' ' + str(ele.is_displayed()))
+                al = ele.get_attribute('aria-label')
+                formatted_start_date = self.start_date_dt.strftime("%A, %B {day}, %Y".format(day=self.start_date_dt.day))
+                if formatted_start_date in al:
+                    print(f'matching date for: {formatted_start_date} in {al}')
+            except Exception:
+                print('issue getting ele ' + str(ele))
+
+        end_element = None
+        while not end_element:  # last_date < self.end_date_dt
+            # elements = self.driver.find_elements(By.XPATH,
+            #                                      '//td[contains(@class, "CalendarDay") and @role="button" and @aria-disabled="false"]')
+            # last_active_date_td = elements[-1]
+            # last_date = get_date_from_aria_label(last_active_date_td)
+            try:
+                print('end date')
+                ele = self.driver.find_element(By.XPATH, f'//td[contains(@aria-label, "{formatted_end_date}")]')
+                print('found ele')
+                print(ele.is_displayed())
+                print(ele.get_attribute('aria-label'))
+                print(ele.get_attribute('outerHTML'))
+                if ele.is_displayed():
+                    end_element = ele
+            except Exception as e:
+                print(e)
+                print('start_element not found or not visible')
+                end_element = None
+            forward_button.click()
+
+        # select the end_date now
+        # end_element = self.driver.find_element(By.XPATH, f'//td[contains(@aria-label, "{formatted_end_date}")]')
+        end_element.click()
+
+        # click on input button again to remove the calendar hover over
+        input_field.click()
+
+        # click the download button
         download_buttons = self.driver.find_elements(By.XPATH,
                                               '//button[contains(., "Download") and @data-testid="op-button"]')
-        print(f'len down buttons: {len(download_buttons)}')
-        for db in download_buttons:
-            print(db.get_attribute('outerHTML'))
-        # except Exception:
-        #     print('exception clause')
-        #     download_button = self.driver.find_element(By.XPATH,
-        #                                           '//button/div[contains(., "Download") and @data-testid="op-button"]')
-        # download_button = self.driver.find_element(By.XPATH,
-        #                                      '//button[contains(text(), "Download")][@data-testid="op-button"]')
-        # download_button = self.wait.until(EC.presence_of_element_located((By.XPATH, "//button[contains(., 'Download')]")))
-        # Scroll the button element into view
         download_button = download_buttons[-1]
-        self.driver.execute_script("arguments[0].removeAttribute('disabled');", download_button)
-        print(download_button.get_attribute('outerHTML'))
-        # self.driver.execute_script("arguments[0].scrollIntoView();", download_button)
-        self.driver.execute_script("arguments[0].click();", download_button)
-        time.sleep(5)
-        # self.wait.until(EC.visibility_of(download_button))
-        # self.wait.until(EC.element_to_be_clickable(download_button))
-
         download_button.click()
 
-        time.sleep(15)
-        return
-        # page_source = self.driver.page_source
-        # print(page_source)
-
-        # Specify the desired month and year
-        desired_month = "April"
-        desired_year = "2023"
-        print('desired')
-        print(desired_month, desired_year)
-
-        # Find the parent element that contains the calendar
-        calendar = self.driver.find_element(By.CSS_SELECTOR, '.CalendarMonthGrid_month__horizontal')
-        print('calendar')
-        print(calendar)
-        # print(calendar.__dict__)
-        # print(calendar.text)
-        print(calendar.get_attribute("outerHTML"))
-        # print(calendar.get_attribute("innerHTML"))
-
-        # Find the back arrow element
-        back_arrow = self.driver.find_element(By.CSS_SELECTOR, '.DayPickerNavigation_button__horizontal_2')
-        print('back arrow')
-        print(back_arrow)
-        # print(back_arrow.__dict__)
-        # print(back_arrow.text)
-        print(back_arrow.get_attribute("outerHTML"))
-        # print(back_arrow.get_attribute("innerHTML"))
-        # back_arrow.click()
-
-        # Loop until the current month and year match the desired values
-        while True:
-            # Get the month and year from the caption element
-            caption = calendar.find_element(By.CSS_SELECTOR, '.CalendarMonth_caption')
-            print('caption')
-            print(caption)
-            print(caption.get_attribute("outerHTML"))
-            # print(caption.get_attribute("innerHTML"))
-
-            soup = BeautifulSoup(caption.get_attribute("innerHTML"), 'html.parser')
-            print('bs4')
-            bs4_text = soup.div.get_text()
-            print(bs4_text)
-
-            print(caption.text)
-            month_year = caption.find_element(By.CSS_SELECTOR, 'div')
-            print('month_year')
-            print(month_year)
-            current_month, current_year = bs4_text.split() or month_year.text.split()
-
-            print('current vals')
-            print(current_month, current_year)
-
-            # Check if the month and year match the desired values
-            if current_month == desired_month and current_year == desired_year:
-                print('match')
-                break
-
-            # Click the back arrow to navigate to the previous month
-            back_arrow.click()
-
-        # Find all the <td> elements within the calendar
-        dates = calendar.find_elements(By.CSS_SELECTOR, 'td.CalendarDay')
-        print(dates)
-
-        # Iterate through each <td> element
-        for date in dates:
-            # Get the day value
-            print(f'date : {date}')
-            print(date.get_attribute("outerHTML"))
-            day = date.find_element(By.CSS_SELECTOR, 'span').text
-            print(day)
-
-            # Select the desired date
-            if day == "1":
-                print('day is 1')
-                # Select the start date
-                # date.click()
-            elif day == "30":
-                # Select the end date
-                print('day is 30')
-                # date.click()
-            # You can add more conditions to handle other specific dates if needed
-
-        start_date = 'April 1, 2023'
-        end_date = 'April 30, 2023'
-        start_date_element = calendar.find_element(By.XPATH, f'.//td[contains(@aria-label, "{start_date}")]')
-
-        end_date_element = calendar.find_element(By.XPATH, f'.//td[contains(@aria-label, "{end_date}")]')
-        print(start_date_element.get_attribute('outerHTML'))
-        print(end_date_element.get_attribute('outerHTML'))
-
-        # Scroll the calendar into view if needed
-        self.driver.execute_script("arguments[0].scrollIntoView(true);", start_date_element)
-
-        # Click on the start date element using JavaScript
-        self.driver.execute_script("arguments[0].click();", start_date_element)
-
-        # Click on the end date element using JavaScript
-        self.driver.execute_script("arguments[0].click();", end_date_element)
-        # date_range_element.send_keys(Keys.ENTER)
-        # start_date_element.click()
-        # end_date_element.click()
-
-
-
-        # After selecting the desired dates, you can proceed with other actions on the page
-
-        # page_source = self.driver.page_source
-        # print(page_source)
-        time.sleep(120)
-        # print(f"Downloaded file(s): {full_filename}")
-        # self.downloaded_files.extend(full_filename)
+        # Wait for the file to be downloaded
+        file_pattern = os.path.join(RAW_REPORTS_PATH, self.ORDER_FILENAME_PATTERN)
+        self.wait.until(lambda driver: any(
+            os.stat(file_path).st_ctime > start_time
+            for file_path in glob.glob(file_pattern)
+        ))
+        # Get the downloaded file(s) that match the condition
+        downloaded_files = [
+            file_path
+            for file_path in glob.glob(file_pattern)
+            if os.stat(file_path).st_ctime > start_time
+        ]
+        print(f"Downloaded file(s): {downloaded_files}")
+        self.downloaded_files.extend(downloaded_files)
 
     def preprocess_reports(self):
         """
@@ -243,22 +224,21 @@ class FutureFoodsOrders(OrdersProvider):
             return
 
         for downloaded_file in self.downloaded_files:
-            pass
-
-        # self.processed_files.append(processed_file)
-        # print(f'Saved {self.store_name} orders to: {processed_file}')
+            processed_file = self.create_processed_filename(ReportType.ORDERS, Extensions.CSV)
+            shutil.copy(downloaded_file, processed_file)
+            self.processed_files.append(processed_file)
+            print(f'Saved {self.store_name} orders to: {processed_file}')
 
     def validate_reports(self):
         """
         Perform report validation specific to the FutureFoods provider.
-        TODO: Compare pdf summaries to transaction summaries.
         """
-        # ValidationUtils.validate_processed_files_count(self.processed_files, 1)
-        # ValidationUtils.validate_downloaded_files_count(self.downloaded_files, 1)
-        # ValidationUtils.validate_processed_files_extension(self.processed_files, Extensions.CSV)
-        # ValidationUtils.validate_downloaded_files_extension(self.downloaded_files, Extensions.CSV)
-        # ValidationUtils.validate_processed_files_date_range(
-        #     self.processed_files, self.start_date, self.end_date, 'Date', '%m/%d/%Y %I:%M %p')
+        ValidationUtils.validate_processed_files_count(self.processed_files, 1)
+        ValidationUtils.validate_downloaded_files_count(self.downloaded_files, 1)
+        ValidationUtils.validate_processed_files_extension(self.processed_files, Extensions.CSV)
+        ValidationUtils.validate_downloaded_files_extension(self.downloaded_files, Extensions.CSV)
+        ValidationUtils.validate_processed_files_date_range(
+            self.processed_files, self.start_date, self.end_date, 'Date', '%Y-%m-%d')
 
         print("Report validation successful")
 
@@ -299,3 +279,6 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+# curl -X POST -F 'client_id=M5VQfGY4x9BjyeOg2XqYQ7cp9xSiHZEp' -F 'client_secret=I7yiL6txSOB0vdR0goWb0PY6BTCphFKTJtE7jvWP' -F 'grant_type=client_credentials' -F "scope=business.receipts" "https://login.uber.com/oauth/v2/token"
